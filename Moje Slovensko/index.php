7cab1cefed6afc8b8145f4c04119cef6c67dcee5
Iamvegant <136906784+Iamvegant@users.noreply.github.com>
2025-08-04 14:36:00 +0200

    Add files via upload

---
+++ Moje Slovensko/index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="sk">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="global.css">
+    <link rel="stylesheet" href="index.css">
+    <title>Document</title>
+</head>
+
+<body>
+    <header>
+        <div id="navbar">
+            <div id="nav-top">
+                <img id="logo" draggable = "false" src="img/logo_final_text.png" alt="logo">
+                <div id="search">
+                    <input id="searchbar" type="text" placeholder="Hľadať">
+                    <button id="search-bt"><img id="mag-glass" src="img/magnifying_glass.png" alt="magnifying_glass"></button>
+                </div>
+                <div id="profileLang">
+                    <button id="profileBT">
+                        <img id="profile-img" draggable = "false" src="img/profile-icon.png" alt="profile-icon">
+                        <div id="profile">
+                            <div id="username">Profil</div>
+                            <div id="login">Prihlasit</div>
+                        </div>
+                    </button>
+                    <a id="lang" href="index.php/en"><img id="flag" draggable="false" src="img/slovakflag.png" alt="slovakflag"></a>
+                </div>
+            </div>
+            <div id="nav-bottom">
+                <a href="">Moje okolie</a>
+                <a href="">Kam na jedlo?</a>
+                <a href="">Kam na túru?</a>
+                <a href="">Kam prespať?</a>
+                <a href="">Prekvap ma</a>
+                <a href="">Nastavenie</a>
+            </div>
+        </div>
+    </header>
+    <main></main>
+    <footer></footer>
+</body>
+
+</html>
